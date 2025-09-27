2f4d5b40c0727fd2849da8e9bbe36ca9562ecdde
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:29:01 -0300

    feat: criado contrato com interface de métodos do repositorio

---
+++ src/Backend/PixelManager.Domain/Repositorios/IMetadadosDeImagemRepository.cs
@@ -0,0 +1,11 @@
+﻿using PixelManager.Domain.Entidades;
+
+namespace PixelManager.Domain.Repositorios;
+public interface IMetadadosDeImagemRepository
+{
+	Task Adicione(MetadadosDeImagem metadados);
+	Task<IList<MetadadosDeImagem>> ConsulteTodos();
+	Task<MetadadosDeImagem?> ConsultePorId(long id);
+	void Atualize(MetadadosDeImagem metadados);
+	Task Remova(long id);
+}
