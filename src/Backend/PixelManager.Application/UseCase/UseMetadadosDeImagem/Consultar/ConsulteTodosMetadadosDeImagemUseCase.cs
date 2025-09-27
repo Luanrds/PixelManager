b0b9bb9fbbf70f35eb2c4811f93884c0a1e10ef4
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:30:16 -0300

    feat: criado usecase para consultar todos

---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
@@ -0,0 +1,22 @@
+﻿using PixelManager.Application.Conversores;
+using PixelManager.Communication.Responses;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Consultar;
+public class ConsulteTodosMetadadosDeImagemUseCase : IConsulteTodosMetadadosDeImagemUseCase
+{
+	private readonly IMetadadosDeImagemRepository _repository;
+
+	public ConsulteTodosMetadadosDeImagemUseCase(IMetadadosDeImagemRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public async Task<List<ResponseMetadadosDeImagemJson>> Execute()
+	{
+		IList<MetadadosDeImagem> metadados = await _repository.ConsulteTodos();
+
+		return metadados.Select(m => m.Converta()).ToList();
+	}
+}
---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Consultar/IConsulteTodosMetadadosDeImagemUseCase.cs
@@ -0,0 +1,7 @@
+﻿using PixelManager.Communication.Responses;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Consultar;
+public interface IConsulteTodosMetadadosDeImagemUseCase
+{
+	Task<List<ResponseMetadadosDeImagemJson>> Execute();
+}
