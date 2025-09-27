07c460a782511359b12b128402b4499ac4314818
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:31:11 -0300

    feat: criado use case para remover

---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Remover/RemovaMetadadosDeImagemUseCase.cs
@@ -0,0 +1,22 @@
+﻿using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+using PixelManager.Exceptions.Exceptions;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Remover;
+internal class RemovaMetadadosDeImagemUseCase : IRemovaMetadadosDeImagemUseCase
+{
+	private readonly IMetadadosDeImagemRepository _repository;
+
+	public RemovaMetadadosDeImagemUseCase(IMetadadosDeImagemRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public async Task Execute(long id)
+	{
+		MetadadosDeImagem? metadados = await _repository.ConsultePorId(id) 
+			?? throw new RecursoNaoEncontradoException();
+
+		await _repository.Remova(id);
+	}
+}
