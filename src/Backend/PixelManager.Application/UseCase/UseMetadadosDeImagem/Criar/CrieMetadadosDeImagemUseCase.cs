aa80710ac0a3ebd5594ac5bbd85ae9f3ce931a47
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:30:53 -0300

    feat: criado use case para criar

---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Criar/CrieMetadadosDeImagemUseCase.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation.Results;
+using PixelManager.Application.Conversores;
+using PixelManager.Application.Validadores;
+using PixelManager.Communication.Request;
+using PixelManager.Communication.Responses;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+using PixelManager.Exceptions.Exceptions;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Criar;
+public class CrieMetadadosDeImagemUseCase : ICrieMetadadosDeImagemUseCase
+{
+	private readonly IMetadadosDeImagemRepository _repository;
+	private readonly MetadadosDeImagemValidator _validator;
+
+	public CrieMetadadosDeImagemUseCase(IMetadadosDeImagemRepository repository, MetadadosDeImagemValidator validator)
+	{
+		_repository = repository;
+		_validator = validator;
+	}
+
+	public async Task<ResponseMetadadosDeImagemJson> Execute(RequestMetadadosDeImagemJson request)
+	{
+		ValidationResult resultado = _validator.Validate(request);
+		if (!resultado.IsValid)
+		{
+			List<string> mensagensDeErro = resultado.Errors.Select(e => e.ErrorMessage).ToList();
+			throw new ErrosDeValidacaoException(mensagensDeErro);
+		}
+
+		MetadadosDeImagem entidade = request.Converta();
+
+		await _repository.Adicione(entidade);
+
+		return entidade.Converta();
+	}
+}
---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Criar/ICrieMetadadosDeImagemUseCase.cs
@@ -0,0 +1,8 @@
+﻿using PixelManager.Communication.Request;
+using PixelManager.Communication.Responses;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Criar;
+public interface ICrieMetadadosDeImagemUseCase
+{
+	Task<ResponseMetadadosDeImagemJson> Execute(RequestMetadadosDeImagemJson request);
+}
