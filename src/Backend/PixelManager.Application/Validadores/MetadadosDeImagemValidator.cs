d09b8fc21e6c6eb1119509264db6b35e4696f725
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:31:30 -0300

    feat: criado validadores

---
+++ src/Backend/PixelManager.Application/Validadores/MetadadosDeImagemValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using PixelManager.Communication.Request;
+
+namespace PixelManager.Application.Validadores;
+public class MetadadosDeImagemValidator : AbstractValidator<RequestMetadadosDeImagemJson>
+{
+	public MetadadosDeImagemValidator()
+	{
+		RuleFor(x => x.NomeDoArquivo)
+			.NotEmpty()
+			.WithMessage("O nome do arquivo é obrigatório.")
+			.MaximumLength(100)
+			.WithMessage("O nome do arquivo não pode exceder 100 caracteres.");
+
+		RuleFor(x => x.TipoDoArquivo)
+			.IsInEnum()
+			.WithMessage("O tipo do arquivo é inválido.");
+
+		RuleFor(x => x.Altura)
+			.GreaterThan(0)
+			.WithMessage("A altura da imagem deve ser maior que zero.");
+
+		RuleFor(x => x.Comprimento)
+			.GreaterThan(0)
+			.WithMessage("O comprimento da imagem deve ser maior que zero.");
+	}
+}
