80713015c4baea25cf87cd05b2fb989e5f0484a6
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:27:33 -0300

    feat: criado database em memória, e criação de injeção de dependencia em método de extensão

---
+++ src/Backend/PixelManager.Infrastructure/Contexto/InMemoryDatabase.cs
@@ -0,0 +1,10 @@
+﻿using PixelManager.Domain.Entidades;
+
+namespace PixelManager.Infrastructure.Contexto;
+public class InMemoryDatabase
+{
+	private static InMemoryDatabase? _instance;
+	public static InMemoryDatabase Instance => _instance ??= new InMemoryDatabase();
+
+	public List<MetadadosDeImagem> MetadadosDeImagens { get; set; } = [];
+}
---
+++ src/Backend/PixelManager.Infrastructure/InjecaoDeDependenciaExtensao.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PixelManager.Domain.Repositorios;
+using PixelManager.Infrastructure.Contexto;
+using PixelManager.Infrastructure.Repositorios;
+
+namespace PixelManager.Infrastructure;
+public static class InjecaoDeDependenciaExtensao
+{
+	public static void AdicioneInfraestrutura(this IServiceCollection services)
+	{
+		services.AddSingleton<InMemoryDatabase>();
+		services.AddSingleton<IMetadadosDeImagemRepository, MetadadosDeImagemRepository>();
+	}
+}
