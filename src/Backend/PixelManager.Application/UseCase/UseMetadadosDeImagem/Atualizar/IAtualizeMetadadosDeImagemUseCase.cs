cda2e8b2614c06f16c36d42629cabb23c15b6fde
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:29:54 -0300

    feat: criado use case para atualizar

---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Atualizar/IAtualizeMetadadosDeImagemUseCase.cs
@@ -0,0 +1,7 @@
+﻿using PixelManager.Communication.Request;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Atualizar;
+public interface IAtualizeMetadadosDeImagemUseCase
+{
+	Task Execute(long id, RequestMetadadosDeImagemJson request);
+}
