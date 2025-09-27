0a665d4fb22dc5c156bcec2f66f63a5526490c6e
Luanrds <luanrsilva209@gmail.com>
2025-09-27 17:30:39 -0300

    feat: criado use case para consultar por id

---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/ConsultarPorId/ConsulteMetadadosDeImagemPorIdUseCase.cs
@@ -0,0 +1,24 @@
+﻿using PixelManager.Application.Conversores;
+using PixelManager.Communication.Responses;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+using PixelManager.Exceptions.Exceptions;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.ConsultarPorId;
+public class ConsulteMetadadosDeImagemPorIdUseCase : IConsulteMetadadosDeImagemPorIdUseCase
+{
+	private readonly IMetadadosDeImagemRepository _repository;
+
+	public ConsulteMetadadosDeImagemPorIdUseCase(IMetadadosDeImagemRepository repository)
+	{
+		_repository = repository;
+	}
+
+	public async Task<ResponseMetadadosDeImagemJson> Execute(long id)
+	{
+		MetadadosDeImagem? metadados = await _repository.ConsultePorId(id) 
+			?? throw new RecursoNaoEncontradoException();
+
+		return metadados.Converta();
+	}
+}
---
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/ConsultarPorId/IConsulteMetadadosDeImagemPorIdUseCase.cs
@@ -0,0 +1,7 @@
+﻿using PixelManager.Communication.Responses;
+
+namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.ConsultarPorId;
+public interface IConsulteMetadadosDeImagemPorIdUseCase
+{
+	Task<ResponseMetadadosDeImagemJson> Execute(long id);
+}
