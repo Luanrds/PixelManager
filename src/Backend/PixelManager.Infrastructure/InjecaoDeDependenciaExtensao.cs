482d9b8f5ff7f2fa08bef1eefb3b1fbe062dea4c
Luanrds <luanrsilva209@gmail.com>
2025-09-28 16:09:33 -0300

    Criação de Teste unitários

--- src/Backend/PixelManager.Infrastructure/InjecaoDeDependenciaExtensao.cs
+++ src/Backend/PixelManager.Infrastructure/InjecaoDeDependenciaExtensao.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PixelManager.Domain.Repositorios;
-using PixelManager.Infrastructure.Contexto;
 using PixelManager.Infrastructure.Repositorios;
 using Raven.Client.Documents;
 using Raven.Embedded;
@@ -12,16 +11,12 @@ public static class InjecaoDeDependenciaExtensao
 	{
 		services.AddSingleton<IDocumentStore>(provider =>
 		{
-			// Inicia o servidor embutido.
-			// Por padrão, ele cria os dados em uma pasta dentro do seu projeto.
 			EmbeddedServer.Instance.StartServer(new ServerOptions
 			{
-				// Define onde os dados do banco serão salvos
 				DataDirectory = "RavenDBData",
-				ServerUrl = "http://127.0.0.1:8080" // Podemos manter o acesso ao Studio
+				ServerUrl = "http://127.0.0.1:8080"
 			});
 
-			// Retorna o DocumentStore do servidor que acabamos de iniciar.
 			return EmbeddedServer.Instance.GetDocumentStore("PixelManagerDB");
 		});
 		services.AddSingleton<IMetadadosDeImagemRepository, MetadadosDeImagemRepository>();
---
+++ tests/Validators.Test/MetadadosDeImagem/Criar/MetadadosDeImagemValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using CommomTestUtilities.Requests;
+using FluentValidation.Results;
+using PixelManager.Application.Validadores;
+
+namespace Validators.Test.MetadadosDeImagem.Criar;
+public class MetadadosDeImagemValidatorTest
+{
+	private readonly MetadadosDeImagemValidator _validator;
+
+	public MetadadosDeImagemValidatorTest(MetadadosDeImagemValidator validator)
+	{
+		_validator = validator;
+	}
+
+	[Fact]
+	public void Success()
+	{
+		var request = RequestMetadadosDeImagemJsonBuilder.Build();
+
+		ValidationResult resultado = _validator.Validate(request);
+
+		Assert.True(resultado.IsValid);
+	}
+}
