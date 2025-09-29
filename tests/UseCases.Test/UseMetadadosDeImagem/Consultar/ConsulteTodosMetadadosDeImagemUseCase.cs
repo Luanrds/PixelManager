7ec4cb704db38e75181a61a4c8b2b11c9f4b9f23
Luanrds <luanrsilva209@gmail.com>
2025-09-28 22:45:47 -0300

    feat: criação de testes para constulra todos

---
+++ tests/UseCases.Test/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
@@ -0,0 +1,44 @@
+﻿using CommomTestUtilities.Repositorios;
+using FluentAssertions;
+using Moq;
+using PixelManager.Application.UseCase.UseMetadadosDeImagem.Consultar;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+
+namespace UseCases.Test.UseMetadadosDeImagem.Consultar;
+public class ConsulteTodosMetadadosDeImagemUseCaseTest
+{
+	[Fact]
+	public async Task Success()
+	{
+		var listaEntidades = new List<MetadadosDeImagem>
+		{
+			new() { Id = "1", NomeDoArquivo = "imagem1.jpg" },
+			new() { Id = "2", NomeDoArquivo = "imagem2.png" }
+		};
+		var (useCase, _) = CreateUseCase(listaEntidades);
+
+		var result = await useCase.Execute();
+
+		result.Should().HaveCount(2);
+	}
+
+	[Fact]
+	public async Task Success_Lista_Vazia()
+	{
+		var (useCase, _) = CreateUseCase([]);
+
+		var result = await useCase.Execute();
+
+		result.Should().BeEmpty();
+	}
+
+	private static (ConsulteTodosMetadadosDeImagemUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(IList<MetadadosDeImagem> entidades)
+	{
+		var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
+		repositoryMock.Setup(r => r.ConsulteTodos()).ReturnsAsync(entidades);
+
+		var useCase = new ConsulteTodosMetadadosDeImagemUseCase(repositoryMock.Object);
+		return (useCase, repositoryMock);
+	}
+}
