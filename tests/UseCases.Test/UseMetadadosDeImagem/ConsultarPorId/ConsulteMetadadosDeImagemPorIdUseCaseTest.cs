f1b9366202a4064c3c9c20606fe512971689b27a
Luanrds <luanrsilva209@gmail.com>
2025-09-28 22:45:20 -0300

    feat: criação de testes para consulta por id

---
+++ tests/UseCases.Test/UseMetadadosDeImagem/ConsultarPorId/ConsulteMetadadosDeImagemPorIdUseCaseTest.cs
@@ -0,0 +1,45 @@
+﻿using CommomTestUtilities.Repositorios;
+using FluentAssertions;
+using Moq;
+using PixelManager.Application.UseCase.UseMetadadosDeImagem.ConsultarPorId;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Repositorios;
+using PixelManager.Exceptions.Exceptions;
+
+namespace UseCases.Test.UseMetadadosDeImagem.ConsultarPorId;
+public class ConsulteMetadadosDeImagemPorIdUseCaseTest
+{
+	[Fact]
+	public async Task Success()
+	{
+		var entidade = new MetadadosDeImagem { Id = "123", NomeDoArquivo = "teste.png" };
+		var (useCase, _) = CreateUseCase(entidade);
+
+		var result = await useCase.Execute(entidade.Id);
+
+		result.Should().NotBeNull();
+		result.Id.Should().Be(entidade.Id);
+		result.NomeDoArquivo.Should().Be(entidade.NomeDoArquivo);
+	}
+
+	[Fact]
+	public async Task Error_Recurso_Nao_Encontrado()
+	{
+		var (useCase, _) = CreateUseCase(null);
+
+		var action = async () => await useCase.Execute("idInexistente");
+
+		await action.Should().ThrowAsync<RecursoNaoEncontradoException>();
+	}
+
+	private static (ConsulteMetadadosDeImagemPorIdUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(MetadadosDeImagem? entidade)
+	{
+		var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
+
+		if (entidade is not null)
+			repositoryMock.Setup(r => r.ConsultePorId(entidade.Id)).ReturnsAsync(entidade);
+
+		var useCase = new ConsulteMetadadosDeImagemPorIdUseCase(repositoryMock.Object);
+		return (useCase, repositoryMock);
+	}
+}
