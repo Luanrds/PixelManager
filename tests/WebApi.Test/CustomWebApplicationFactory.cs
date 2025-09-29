effb27a357fed81600c6232e229f61acdc9400bd
Luanrds <luanrsilva209@gmail.com>
2025-09-28 21:17:00 -0300

    criado testes de integração com banco de dados local

---
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Raven.Client.Documents;
+using Raven.Embedded;
+using Raven.TestDriver;
+
+namespace WebApi.Test;
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+	private IDocumentStore? _store;
+
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		builder.UseEnvironment("Test");
+
+		builder.ConfigureServices(services =>
+		{
+			var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IDocumentStore));
+			if (descriptor != null)
+			{
+				services.Remove(descriptor);
+			}
+
+			var testDriver = new RavenTestDriverWrapper();
+			_store = testDriver.GetDocumentStore();
+			services.AddSingleton<IDocumentStore>(_store);
+		});
+	}
+
+	private class RavenTestDriverWrapper : RavenTestDriver
+	{
+		static RavenTestDriverWrapper()
+		{
+			ConfigureServer(new TestServerOptions
+			{
+				Licensing = new ServerOptions.LicensingOptions
+				{
+					ThrowOnInvalidOrMissingLicense = false
+				}
+			});
+		}
+
+		public IDocumentStore GetDocumentStore()
+		{
+			return base.GetDocumentStore();
+		}
+	}
+}
---
+++ tests/WebApi.Test/MetadadosDeImagem/Criar/CrieMetadadosDeImagem.cs
@@ -0,0 +1,49 @@
+﻿using CommomTestUtilities.Requests;
+using FluentAssertions;
+using PixelManager.Communication.Responses;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace WebApi.Test.MetadadosDeImagem.Criar;
+public class CrieMetadadosDeImagem : IClassFixture<CustomWebApplicationFactory>
+{
+	private readonly HttpClient _httpClient;
+
+	public CrieMetadadosDeImagem(CustomWebApplicationFactory factory)
+	{
+		_httpClient = factory.CreateClient();
+	}
+
+	[Fact]
+	public async Task Success()
+	{
+		var request = RequestMetadadosDeImagemJsonBuilder.Build();
+
+		var response = await _httpClient.PostAsJsonAsync("MetadadosDeImagem", request);
+
+		response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+		var responseBody = await response.Content.ReadFromJsonAsync<ResponseMetadadosDeImagemJson>();
+
+		responseBody.Should().NotBeNull();
+		responseBody.NomeDoArquivo.Should().Be(request.NomeDoArquivo);
+		responseBody.Id.Should().NotBeEmpty();
+	}
+
+	[Fact]
+	public async Task Error_Request_Invalido()
+	{
+		var request = RequestMetadadosDeImagemJsonBuilder.Build();
+		request.NomeDoArquivo = string.Empty;
+
+		var response = await _httpClient.PostAsJsonAsync("MetadadosDeImagem", request);
+
+		response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+		var responseBody = await response.Content.ReadFromJsonAsync<ResponseErros>();
+
+		responseBody.Should().NotBeNull();
+		responseBody.Errors.Should().ContainSingle()
+			.And.Contain("O nome do arquivo é obrigatório.");
+	}
+}
