ac0e680baf45c7b7d1ae0c6431df4114c9e1be9f
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-21 11:04:24 -0300

    Adição e refatoração de métodos para consulta de dados específicos ou não

--- src/Backend/PixelManager.Infrastructure/Repositorios/MetadadosDeImagemRepository.cs
+++ src/Backend/PixelManager.Infrastructure/Repositorios/MetadadosDeImagemRepository.cs
@@ -1,6 +1,9 @@
-﻿using PixelManager.Domain.Entidades;
+﻿using PixelManager.Domain.Dto;
+using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Extensoes;
 using PixelManager.Domain.Repositorios;
 using Raven.Client.Documents;
+using Raven.Client.Documents.Linq;
 using Raven.Client.Documents.Session;
 
 namespace PixelManager.Infrastructure.Repositorios;
@@ -34,14 +37,51 @@ public class MetadadosDeImagemRepository(IDocumentStore store) : IMetadadosDeIma
 		return await session.Query<MetadadosDeImagem>().ToListAsync();
 	}
 
-	public async Task Remova(string id)
-	{
-		MetadadosDeImagem? metadados = await ConsultePorId(id);
-		if (metadados != null)
-		{
-			using IAsyncDocumentSession session = _store.OpenAsyncSession();
-			session.Delete(id);
-			await session.SaveChangesAsync();
-		}
-	}
+    public async Task Remova(string id)
+    {
+        MetadadosDeImagem? metadados = await ConsultePorId(id);
+        if (metadados != null)
+        {
+            using IAsyncDocumentSession session = _store.OpenAsyncSession();
+            session.Delete(id);
+            await session.SaveChangesAsync();
+        }
+    }
+
+    public T ObterTodos<T>(Func<IDocumentSession, T> execucao)
+    {
+        return Obter(execucao);
+    }
+
+    protected T Obter<T>(Func<IDocumentSession, T> execucao)
+    {
+        using var session = _store.OpenSession();
+        return execucao.Invoke(session);
+    }
+
+    public ListaPaginada<MetadadosDeImagem> ObterPorFiltroPaginacao(IDocumentSession sessao, DtoFiltromMetadadosDeImagem filtro)
+    {
+        var query = ObterQueryPor(sessao, filtro);
+        return query.ObterPaginacao(sessao, filtro);
+    }
+
+    private IRavenQueryable<MetadadosDeImagem> ObterQueryPor(IDocumentSession sessao, DtoFiltromMetadadosDeImagem? filtro = null)
+    {
+        filtro ??= new();
+        var query = sessao.Query<MetadadosDeImagem>();
+
+        if (!string.IsNullOrWhiteSpace(filtro.NomeDoArquivo))
+            query = query.Search(x => x.NomeDoArquivo, filtro.NomeDoArquivo);
+
+        if (filtro.TipoDoArquivo.HasValue)
+            query = query.Where(x => x.TipoDoArquivo == filtro.TipoDoArquivo.Value);
+
+        //if (filtro.DataDeCriacaoInicial.HasValue)
+        //    query = query.Where(x => x.DataDeCriacao >= filtro.DataDeCriacaoInicial.Value);
+
+        //if (filtro.DataDeCriacaoFinal.HasValue)
+        //    query = query.Where(x => x.DataDeCriacao <= filtro.DataDeCriacaoFinal.Value);
+
+        return query;
+    }
 }
