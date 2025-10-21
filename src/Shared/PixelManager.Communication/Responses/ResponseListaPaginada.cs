a8f7a53780865bebce755aea1656bf06587d1acd
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-21 13:05:09 -0300

    Criação de response para paginação

---
+++ src/Shared/PixelManager.Communication/Responses/ResponseListaPaginada.cs
@@ -0,0 +1,10 @@
+﻿namespace PixelManager.Communication.Responses;
+
+public class ResponseListaPaginada<T>
+{
+    public List<T> Dados { get; set; } = [];
+    public int QuantidadeTotal { get; set; }
+    public int TotalFiltrado { get; set; }
+    public int Pagina { get; set; }
+    public int LinhasPorPagina { get; set; }
+}
