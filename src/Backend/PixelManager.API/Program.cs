cb49016b0d97a26654852222e338dc43002e2004
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-21 11:07:51 -0300

    Criação de CORS para comunicação com front

--- src/Backend/PixelManager.API/Program.cs
+++ src/Backend/PixelManager.API/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options =>
 {
-	options.Filters.Add(typeof(ExceptionFilter));
+    options.Filters.Add(typeof(ExceptionFilter));
 });
 
 builder.Services.AdicioneAplicacao();
@@ -16,15 +16,27 @@ builder.Services.AdicioneInfraestrutura();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:8080")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PermitirFrontend");
 
 app.UseAuthorization();
 
@@ -34,5 +46,5 @@ app.Run();
 
 public partial class Program
 {
-	protected Program() { }
+    protected Program() { }
 }
