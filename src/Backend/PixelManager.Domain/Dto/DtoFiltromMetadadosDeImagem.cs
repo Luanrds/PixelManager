d1717796938c824c2972e983259652cf4d9300e2
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-21 11:06:55 -0300

    criação de dto

---
+++ src/Backend/PixelManager.Domain/Dto/DtoFiltromMetadadosDeImagem.cs
@@ -0,0 +1,12 @@
+﻿using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Enum;
+
+namespace PixelManager.Domain.Dto;
+
+public class DtoFiltromMetadadosDeImagem : FiltroBaseCarregarMais
+{
+    public string NomeDoArquivo { get; set; } = string.Empty;
+    public TipoArquivo? TipoDoArquivo { get; set; }
+    public DateTime? DataDeCriacaoInicial { get; set; }
+    public DateTime? DataDeCriacaoFinal { get; set; }
+}
