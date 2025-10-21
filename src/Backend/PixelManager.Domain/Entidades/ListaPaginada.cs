c0c3d28d4bb45b67f14ef47ac46f942e08219e4d
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-21 11:05:15 -0300

    Criação de lógica de paginação

---
+++ src/Backend/PixelManager.Domain/Entidades/ListaPaginada.cs
@@ -0,0 +1,28 @@
+﻿using PixelManager.Domain.Interfaces;
+
+namespace PixelManager.Domain.Entidades;
+
+public class ListaPaginada<T> : IPaginacao
+{
+    public const int LINHAS_POR_PAGINA = 10;
+    public const int PAGINA_PADRAO = 1;
+
+    private int linhasPorPagina;
+    public int LinhasPorPagina
+    {
+        get => linhasPorPagina;
+        set => linhasPorPagina = value == default(int) ? LINHAS_POR_PAGINA : value;
+    }
+
+    private int pagina;
+    public int Pagina
+    {
+        get => pagina;
+        set => pagina = value == default(int) ? PAGINA_PADRAO : value;
+    }
+
+    public List<T> Dados { get; set; }
+    public int QuantidadeTotal { get; set; }
+    public int TotalFiltrado { get; set; }
+    public Dictionary<string, object> DadosAdicionais { get; set; } = [];
+}
---
+++ src/Backend/PixelManager.Domain/Interfaces/IPaginacao.cs
@@ -0,0 +1,7 @@
+﻿namespace PixelManager.Domain.Interfaces;
+
+public interface IPaginacao
+{
+    int LinhasPorPagina { get; set; }
+    int Pagina { get; set; }
+}
