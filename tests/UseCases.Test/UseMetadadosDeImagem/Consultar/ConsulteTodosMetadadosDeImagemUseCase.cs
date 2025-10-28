21b5c92f946de8918064b94cfec3f07ae86f0927
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 15:32:55 -0300

    Ajuste nos teste de consulta para tornar assíncrono.

--- tests/UseCases.Test/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
+++ tests/UseCases.Test/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
@@ -9,39 +9,43 @@ using PixelManager.Domain.Repositorios;
 namespace UseCases.Test.UseMetadadosDeImagem.Consultar;
 public class ConsulteTodosMetadadosDeImagemUseCaseTest
 {
-    //[Fact]
-    //public void Success()
-    //{
-    //    var listaEntidades = new List<MetadadosDeImagem>
-    //    {
-    //        new() { Id = "1", NomeDoArquivo = "imagem1.jpg" },
-    //        new() { Id = "2", NomeDoArquivo = "imagem2.png" }
-    //    };
-    //    var (useCase, _) = CreateUseCase(listaEntidades);
-    //    var filtro = new DtoFiltromMetadadosDeImagem();
+    [Fact]
+    public async Task Success()
+    {
+        var listaEntidades = new List<MetadadosDeImagem>
+    {
+        new() { Id = "1", NomeDoArquivo = "imagem1.jpg" },
+        new() { Id = "2", NomeDoArquivo = "imagem2.png" }
+    };
+        var (useCase, _) = CreateUseCase(listaEntidades);
+        var filtro = new DtoFiltromMetadadosDeImagem();
 
-    //    var result = useCase.Execute(filtro);
+        var result = await useCase.ExecuteAsync(filtro);
 
-    //    result.Should().HaveCount(2);
-    //}
+        result.Should().HaveCount(2);
+    }
 
-    //[Fact]
-    //public void Success_Lista_Vazia()
-    //{
-    //    var (useCase, _) = CreateUseCase([]);
-    //    var filtro = new DtoFiltromMetadadosDeImagem();
+    [Fact]
+    public async Task Success_Lista_Vazia()
+    {
+        var (useCase, _) = CreateUseCase([]);
+        var filtro = new DtoFiltromMetadadosDeImagem();
 
-    //    var result = useCase.Execute(filtro);
+        var result = await useCase.ExecuteAsync(filtro);
 
-    //    result.Should().BeEmpty();
-    //}
+        result.Should().BeEmpty();
+    }
 
-    //private static (ConsulteTodosMetadadosDeImagemUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(IList<MetadadosDeImagem> entidades)
-    //{
-    //    var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
-    //    repositoryMock.Setup(r => r.ConsulteTodos()).ReturnsAsync(entidades);
+    private static (ConsulteTodosMetadadosDeImagemUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(IList<MetadadosDeImagem> entidades)
+    {
+        var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
 
-    //    var useCase = new ConsulteTodosMetadadosDeImagemUseCase(repositoryMock.Object);
-    //    return (useCase, repositoryMock);
-    //}
+        repositoryMock
+            .Setup(r => r.ConsultePorFiltroAsync(It.IsAny<DtoFiltromMetadadosDeImagem>()))
+            .ReturnsAsync(entidades);
+
+        var useCase = new ConsulteTodosMetadadosDeImagemUseCase(repositoryMock.Object);
+
+        return (useCase, repositoryMock);
+    }
 }
