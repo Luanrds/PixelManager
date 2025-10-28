e105eaa70b9682a27d04695ffdb3be7194d57f7d
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 16:34:07 -0300

    Ajuste em espaços

--- src/Backend/PixelManager.API/Controllers/MetadadosDeImagemController.cs
+++ src/Backend/PixelManager.API/Controllers/MetadadosDeImagemController.cs
@@ -12,25 +12,25 @@ namespace PixelManager.API.Controllers;
 
 public class MetadadosDeImagemController : PixelManagerBaseController
 {
-    [HttpPost]
-    [ProducesResponseType(typeof(ResponseMetadadosDeImagemJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(typeof(ResponseErros), StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> Crie(
-        [FromServices] ICrieMetadadosDeImagemUseCase useCase,
-        [FromBody] RequestMetadadosDeImagemJson request)
-    {
-        ResponseMetadadosDeImagemJson response = await useCase.Execute(request);
-        return Created(string.Empty, response);
-    }
+	[HttpPost]
+	[ProducesResponseType(typeof(ResponseMetadadosDeImagemJson), StatusCodes.Status201Created)]
+	[ProducesResponseType(typeof(ResponseErros), StatusCodes.Status204NoContent)]
+	public async Task<IActionResult> Crie(
+		[FromServices] ICrieMetadadosDeImagemUseCase useCase,
+		[FromBody] RequestMetadadosDeImagemJson request)
+	{
+		ResponseMetadadosDeImagemJson response = await useCase.Execute(request);
+		return Created(string.Empty, response);
+	}
 
     [HttpGet]
     [ProducesResponseType(typeof(List<ResponseMetadadosDeImagemJson>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ResponseErros), StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> ConsulteTodos(
-        [FromServices] IConsulteTodosMetadadosDeImagemUseCase useCase,
-        [FromQuery] DtoFiltromMetadadosDeImagem filtro)
+    public IActionResult ConsulteTodos(
+		[FromServices] IConsulteTodosMetadadosDeImagemUseCase useCase,
+		[FromQuery] DtoFiltromMetadadosDeImagem filtro)
     {
-        var response = await useCase.ExecuteAsync(filtro);
+        List<ResponseMetadadosDeImagemJson> response = useCase.Execute(filtro);
 
         if (response.Count != 0)
         {
@@ -41,39 +41,39 @@ public class MetadadosDeImagemController : PixelManagerBaseController
     }
 
     [HttpGet]
-    [Route("{*id}")]
-    [ProducesResponseType(typeof(ResponseMetadadosDeImagemJson), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> ConsultePorId(
-    [FromServices] IConsulteMetadadosDeImagemPorIdUseCase useCase,
-    [FromRoute] string id)
-    {
-        ResponseMetadadosDeImagemJson response = await useCase.Execute(id);
-        return Ok(response);
-    }
+	[Route("{*id}")]
+	[ProducesResponseType(typeof(ResponseMetadadosDeImagemJson), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> ConsultePorId(
+	[FromServices] IConsulteMetadadosDeImagemPorIdUseCase useCase,
+	[FromRoute] string id)
+	{
+		ResponseMetadadosDeImagemJson response = await useCase.Execute(id);
+		return Ok(response);
+	}
 
-    [HttpPut]
-    [Route("{*id}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> Atualize(
-    [FromServices] IAtualizeMetadadosDeImagemUseCase useCase,
-    [FromRoute] string id,
-    [FromBody] RequestMetadadosDeImagemJson request)
-    {
-        await useCase.Execute(id, request);
-        return NoContent();
-    }
+	[HttpPut]
+	[Route("{*id}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Atualize(
+	[FromServices] IAtualizeMetadadosDeImagemUseCase useCase,
+	[FromRoute] string id,
+	[FromBody] RequestMetadadosDeImagemJson request)
+	{
+		await useCase.Execute(id, request);
+		return NoContent();
+	}
 
-    [HttpDelete]
-    [Route("{*id}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> Remove(
-    [FromServices] IRemovaMetadadosDeImagemUseCase useCase,
-    [FromRoute] string id)
-    {
-        await useCase.Execute(id);
-        return NoContent();
-    }
+	[HttpDelete]
+	[Route("{*id}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(typeof(ResponseErros), StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> Remove(
+	[FromServices] IRemovaMetadadosDeImagemUseCase useCase,
+	[FromRoute] string id)
+	{
+		await useCase.Execute(id);
+		return NoContent();
+	}
 }
