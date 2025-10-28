eab7ca0414f137570558b55c41adb33c7206c27b
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 20:34:03 -0300

    Adicionar suporte a filtro no IMetadadosDeImagemRepository
    Incluído método ConsultePorFiltroAsync para consultas com critérios de filtragem.

--- src/Backend/PixelManager.Domain/Repositorios/IMetadadosDeImagemRepository.cs
+++ src/Backend/PixelManager.Domain/Repositorios/IMetadadosDeImagemRepository.cs
@@ -1,11 +1,13 @@
-﻿using PixelManager.Domain.Entidades;
+﻿using PixelManager.Domain.Dto;
+using PixelManager.Domain.Entidades;
 
 namespace PixelManager.Domain.Repositorios;
 public interface IMetadadosDeImagemRepository
 {
-	Task Adicione(MetadadosDeImagem metadados);
-	Task<IList<MetadadosDeImagem>> ConsulteTodos();
-	Task<MetadadosDeImagem?> ConsultePorId(string id);
-	Task Atualize(MetadadosDeImagem metadados);
-	Task Remova(string id);
+    Task Adicione(MetadadosDeImagem metadados);
+    Task Atualize(MetadadosDeImagem metadados);
+    Task<MetadadosDeImagem?> ConsultePorId(string id);
+    Task<IList<MetadadosDeImagem>> ConsulteTodos();
+    Task<IList<MetadadosDeImagem>> ConsultePorFiltroAsync(DtoFiltroMetadadosDeImagem filtro);
+    Task Remova(string id);
 }
