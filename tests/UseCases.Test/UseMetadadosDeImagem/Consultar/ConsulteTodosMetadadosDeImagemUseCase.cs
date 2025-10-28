de357ee50cb2b6295acb3fdd58b987de0317f743
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 20:42:43 -0300

    Ajustar use case e testes para suportar filtragem
    Atualizados testes e mocks para utilizar ConsultePorFiltroAsync e receber DtoFiltroMetadadosDeImagem.

--- tests/UseCases.Test/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
+++ tests/UseCases.Test/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
@@ -2,43 +2,50 @@
 using FluentAssertions;
 using Moq;
 using PixelManager.Application.UseCase.UseMetadadosDeImagem.Consultar;
+using PixelManager.Domain.Dto;
 using PixelManager.Domain.Entidades;
 using PixelManager.Domain.Repositorios;
 
 namespace UseCases.Test.UseMetadadosDeImagem.Consultar;
 public class ConsulteTodosMetadadosDeImagemUseCaseTest
 {
-	[Fact]
-	public async Task Success()
-	{
-		var listaEntidades = new List<MetadadosDeImagem>
-		{
-			new() { Id = "1", NomeDoArquivo = "imagem1.jpg" },
-			new() { Id = "2", NomeDoArquivo = "imagem2.png" }
-		};
-		var (useCase, _) = CreateUseCase(listaEntidades);
+    [Fact]
+    public async Task Success()
+    {
+        var listaEntidades = new List<MetadadosDeImagem>
+    {
+        new() { Id = "1", NomeDoArquivo = "imagem1.jpg" },
+        new() { Id = "2", NomeDoArquivo = "imagem2.png" }
+    };
+        var (useCase, _) = CreateUseCase(listaEntidades);
+        var filtro = new DtoFiltroMetadadosDeImagem();
 
-		var result = await useCase.Execute();
+        var result = await useCase.Execute(filtro);
 
-		result.Should().HaveCount(2);
-	}
+        result.Should().HaveCount(2);
+    }
 
-	[Fact]
-	public async Task Success_Lista_Vazia()
-	{
-		var (useCase, _) = CreateUseCase([]);
+    [Fact]
+    public async Task Success_Lista_Vazia()
+    {
+        var (useCase, _) = CreateUseCase([]);
+        var filtro = new DtoFiltroMetadadosDeImagem();
 
-		var result = await useCase.Execute();
+        var result = await useCase.Execute(filtro);
 
-		result.Should().BeEmpty();
-	}
+        result.Should().BeEmpty();
+    }
 
-	private static (ConsulteTodosMetadadosDeImagemUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(IList<MetadadosDeImagem> entidades)
-	{
-		var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
-		repositoryMock.Setup(r => r.ConsulteTodos()).ReturnsAsync(entidades);
+    private static (ConsulteTodosMetadadosDeImagemUseCase, Mock<IMetadadosDeImagemRepository>) CreateUseCase(IList<MetadadosDeImagem> entidades)
+    {
+        var repositoryMock = MetadadosDeImagemRepositorybuilder.Build();
 
-		var useCase = new ConsulteTodosMetadadosDeImagemUseCase(repositoryMock.Object);
-		return (useCase, repositoryMock);
-	}
+        repositoryMock
+            .Setup(r => r.ConsultePorFiltroAsync(It.IsAny<DtoFiltroMetadadosDeImagem>()))
+            .ReturnsAsync(entidades);
+
+        var useCase = new ConsulteTodosMetadadosDeImagemUseCase(repositoryMock.Object);
+
+        return (useCase, repositoryMock);
+    }
 }
