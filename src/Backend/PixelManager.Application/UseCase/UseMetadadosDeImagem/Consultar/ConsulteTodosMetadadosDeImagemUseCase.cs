bd6f9fdf4c5ff62ab235bee0bd0547e75841bc1c
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 17:07:14 -0300

    Ajuste nos espaços

--- src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
+++ src/Backend/PixelManager.Application/UseCase/UseMetadadosDeImagem/Consultar/ConsulteTodosMetadadosDeImagemUseCase.cs
@@ -1,10 +1,10 @@
 ﻿using PixelManager.Application.Conversores;
 using PixelManager.Communication.Responses;
 using PixelManager.Domain.Dto;
+using PixelManager.Domain.Entidades;
 using PixelManager.Domain.Repositorios;
 
 namespace PixelManager.Application.UseCase.UseMetadadosDeImagem.Consultar;
-
 public class ConsulteTodosMetadadosDeImagemUseCase : IConsulteTodosMetadadosDeImagemUseCase
 {
     private readonly IMetadadosDeImagemRepository _repository;
@@ -14,10 +14,11 @@ public class ConsulteTodosMetadadosDeImagemUseCase : IConsulteTodosMetadadosDeIm
         _repository = repository;
     }
 
-    public async Task<List<ResponseMetadadosDeImagemJson>> ExecuteAsync(DtoFiltromMetadadosDeImagem filtro)
+    public List<ResponseMetadadosDeImagemJson> Execute(DtoFiltromMetadadosDeImagem filtro)
     {
-        var metadadosFiltrados = await _repository.ConsultePorFiltroAsync(filtro);
+        ListaPaginada<MetadadosDeImagem> metadados = 
+            _repository.ObterTodos(sessao => _repository.ObterPorFiltroPaginacao(sessao, filtro));
 
-        return metadadosFiltrados.Select(m => m.Converta()).ToList();
+        return metadados.Dados.Select(m => m.Converta()).ToList();
     }
 }
