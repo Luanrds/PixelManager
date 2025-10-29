10fc08a295835c93893da152cb5a5565c999b34b
Luan Rodrigues <luanrsilva209@gmail.com>
2025-10-28 21:25:16 -0300

    Adicionar DtoFiltroMetadadosDeImagem para filtros de metadados
    DTO criado para encapsular critérios de filtro por nome, tipo e intervalo de datas

---
+++ src/Backend/PixelManager.Domain/Dto/DtoFiltroMetadadosDeImagem.cs
@@ -0,0 +1,11 @@
+﻿using PixelManager.Domain.Enum;
+
+namespace PixelManager.Domain.Dto;
+
+public class DtoFiltroMetadadosDeImagem
+{
+    public string NomeDoArquivo { get; set; } = string.Empty;
+    public IList<TipoArquivo>? TiposDoArquivo { get; set; }
+    public DateTime? DataDeCriacaoInicial { get; set; }
+    public DateTime? DataDeCriacaoFinal { get; set; }
+}
