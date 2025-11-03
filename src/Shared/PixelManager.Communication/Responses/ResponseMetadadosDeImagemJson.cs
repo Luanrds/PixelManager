e54608b11026ca2abf89686da2946adfd1f3dff1
Luan Rodrigues <luanrsilva209@gmail.com>
2025-11-03 17:30:59 -0300

    Ajuste na separação de métodos de conversão

--- src/Shared/PixelManager.Communication/Responses/ResponseMetadadosDeImagemJson.cs
+++ src/Shared/PixelManager.Communication/Responses/ResponseMetadadosDeImagemJson.cs
@@ -1,13 +1,26 @@
-﻿using PixelManager.Domain.Enum;
+﻿using PixelManager.Domain.Entidades;
+using PixelManager.Domain.Enum;
 
 namespace PixelManager.Communication.Responses;
 public class ResponseMetadadosDeImagemJson
 {
-	public string Id { get; set; } = string.Empty;
-	public string NomeDoArquivo { get; set; } = string.Empty;
-	public TipoArquivo TipoDoArquivo { get; set; }
-	public int Altura { get; set; }
-	public int Comprimento { get; set; }
-	public float Proporcao { get; set; }
-	public DateTime DataDeCriacao { get; set; }
+    public string Id { get; set; } = string.Empty;
+    public string NomeDoArquivo { get; set; } = string.Empty;
+    public TipoArquivo TipoDoArquivo { get; set; }
+    public int Altura { get; set; }
+    public int Comprimento { get; set; }
+    public float Proporcao { get; set; }
+    public DateTime DataDeCriacao { get; set; }
+
+    public static ResponseMetadadosDeImagemJson ConvertaDe(MetadadosDeImagem entidade) =>
+        new()
+        {
+            Id = entidade.Id,
+            NomeDoArquivo = entidade.NomeDoArquivo,
+            TipoDoArquivo = entidade.TipoDoArquivo,
+            Altura = entidade.Altura,
+            Comprimento = entidade.Comprimento,
+            Proporcao = entidade.Proporcao,
+            DataDeCriacao = entidade.DataDeCriacao
+        };
 }
